a101049455831c1b49257039fa1ab01101980bae
Omkar Vasant Nate <nate.o@northeastern.edu>
2024-03-20 20:40:11 -0400

    Fix log file permission

--- packer/setup.sh
+++ packer/setup.sh
@@ -32,8 +32,9 @@ sudo mv /tmp/config.yaml /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
 
 echo "======================== CREATE LOG FILE ========================"
+sudo mkdir /var/log/webapp
 sudo touch /var/log/webapp/webapp.log
-sudo chown csye6225:csye6225 /var/log/webapp/webapp.log
+sudo chown csye6225:csye6225 /var/log/webapp
 
 
 #echo "======================== INSTALLING POSTGRESQL ========================"
